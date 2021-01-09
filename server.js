415216839e41208840dbe737b1b16e15230a9a2b
selinahsu <selinaraindrop@gmail.com>
2021-01-09 04:43:11 -0500

    places api works

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-const path = require('path');
+const restaurantRoutes = require('./routes/restaurants');
 
 const app = express();
+
+// use restaurantRoutes endpoints
+app.use(restaurantRoutes); 
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
